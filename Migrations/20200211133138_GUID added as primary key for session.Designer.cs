57d2a971bc42cc489275514b7cd225617ddb57ba
aneskurtovic <anes.kurtovic@edu.fit.ba>
2020-02-12 12:47:27 +0100

    Changed SessionID from int to GUID

--- Migrations/20200211133138_GUID added as primary key for session.Designer.cs
+++ Migrations/20200211133138_GUID added as primary key for session.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Quizard.API.Data;
 
 namespace Quizard.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200211133138_GUID added as primary key for session")]
+    partial class GUIDaddedasprimarykeyforsession
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200211133138_GUID added as primary key for session.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quizard.API.Migrations
+{
+    public partial class GUIDaddedasprimarykeyforsession : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                 name: "Sessions");
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false, defaultValueSql: "newsequentialid()"),
+                    QuizId = table.Column<int>(nullable: false),
+                    StartedAt = table.Column<DateTime>(nullable: false),
+                    FinishedAt = table.Column<DateTime>(nullable: false),
+                    ContestantName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                column: "ConcurrencyStamp",
+                value: "fd2383ae-250c-4575-a8ee-5a93d95412f0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "dd7cbbd4-d05f-4e14-a1f5-b05704105ce3", "AQAAAAEAACcQAAAAEDIqY7GugEcr4rflDFABX9BZA05/cGmgyisAROok0Mvg3zoEeQMYvJotPCocjlw47w==" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Sessions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                column: "ConcurrencyStamp",
+                value: "3e745a4d-19b3-4ca9-bdf2-4eeca3188005");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "123592c9-8d6b-4c23-902a-3a9ed6462c22", "AQAAAAEAACcQAAAAEL4Q6mhPirXHosANs/yASW+oBBLDHiR8LnabivqdyY/vIZbeNYOTxMKmLnNQgPKMYg==" });
+        }
+    }
+}
