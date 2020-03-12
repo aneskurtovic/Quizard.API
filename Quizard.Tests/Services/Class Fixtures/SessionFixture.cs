30e52a82306ee76048309862f510c8199d8be600
aneskurtovic <anes.kurtovic@edu.fit.ba>
2020-03-12 12:36:20 +0100

    Added class fixture and collection fixture for session

---
+++ Quizard.Tests/Services/Class Fixtures/SessionFixture.cs
@@ -0,0 +1,18 @@
+﻿using FakeItEasy;
+using Quizard.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizard.Tests.Services.Class_Fixtures
+{
+    public class SessionFixture
+    {
+        public ISessionRepository _repo { get; private set; }
+
+        public SessionFixture()
+        {
+            _repo = A.Fake<ISessionRepository>();
+        }
+    }
+}
--- Quizard.Tests/Services/SessionServiceTests.cs
+++ Quizard.Tests/Services/SessionServiceTests.cs
@@ -1,80 +1,64 @@
-﻿using FakeItEasy;
-using FluentAssertions;
-using Quizard.API.Data;
+﻿using FluentAssertions;
 using Quizard.API.Dtos;
-using Quizard.API.Models;
 using Quizard.API.Services;
 using Quizard.Tests.Builders;
+using Quizard.Tests.Services.Class_Fixtures;
+using Quizard.Tests.Services.Collection_Fixtures;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Quizard.Tests.Services
 {
-    public class SessionServiceTests : BaseServiceTest
+    [Collection("Mapper collection")]
+    public class SessionServiceTests : IClassFixture<SessionFixture>
     {
-        public SessionServiceTests() : base()
-        {
+        SessionFixture mockRepository;
+        MapperFixture mapperFixture;
 
+        public SessionServiceTests(SessionFixture _mockRepository, MapperFixture mapper)
+        {
+            mockRepository = _mockRepository;
+            mapperFixture = mapper;
         }
 
         [Fact]
         public async Task GivenNoContestantNameInDto_WhenAddSessionInvoked_ShouldThrowException()
         {
-            //Given
-            var mockRepository = A.Fake<ISessionRepository>();
-            var service = new SessionService(mockRepository, _mapper);
+            var service = new SessionService(mockRepository._repo, mapperFixture._mapper);
 
-            //When
             Func<Task> action = async () => await service.AddSession(new CreateSessionDtoBuilder().BuildQuizId(1).Build());
 
-
-            //Then 
             await action.Should().ThrowAsync<Exception>().WithMessage("Contestant name cannot be empty.");
         }
 
         [Fact]
         public async Task GivenNoQuizId_WhenAddSessionInvoked_ShouldThrowException()
         {
-            //Given
-            var mockRepository = A.Fake<ISessionRepository>();
-            var service = new SessionService(mockRepository, _mapper);
+            var service = new SessionService(mockRepository._repo, mapperFixture._mapper);
 
-            //When
             Func<Task> action = async () => await service.AddSession(new CreateSessionDtoBuilder().BuildContestantName("Contestant").Build());
 
-            //Then 
             await action.Should().ThrowAsync<Exception>().WithMessage("Quiz Id cannot be lesser than 1.");
         }
 
         [Fact]
         public async Task GivenQuizIdZero_WhenAddSessionInvoked_ShouldThrowException()
         {
-            //Given
-            var mockRepository = A.Fake<ISessionRepository>();
-            var service = new SessionService(mockRepository, _mapper);
+            var service = new SessionService(mockRepository._repo, mapperFixture._mapper);
 
-            //When
             Func<Task> action = async () => await service.AddSession(new CreateSessionDtoBuilder().BuildContestantName("Contestant").BuildQuizId(0).Build());
 
-            //Then
             await action.Should().ThrowAsync<Exception>().WithMessage("Quiz Id cannot be lesser than 1.");
-
-
         }
+
         [Fact]
         public async Task GivenQuizIdAndContestantName_WhenAddSessionInvoked_ShouldNotBeNull()
         {
-            //Given
-            var mockRepository = A.Fake<ISessionRepository>();
-            var service = new SessionService(mockRepository, _mapper);
+            var service = new SessionService(mockRepository._repo, mapperFixture._mapper);
 
-            //When
             var result = await service.AddSession(new CreateSessionDtoBuilder().BuildContestantName("Contestant").BuildQuizId(2).Build());
 
-            //Then 
             result.Should().BeEquivalentTo(new ResponseSessionDto
             {
                 QuizId = result.QuizId
@@ -84,32 +68,22 @@ namespace Quizard.Tests.Services
         [Fact]
         public async Task GivenNoSessionId_WhenGetSession_ShouldThrowException()
         {
-            //Given
-            var mockRepository = A.Fake<ISessionRepository>();
-            var service = new SessionService(mockRepository, _mapper);
+            var service = new SessionService(mockRepository._repo, mapperFixture._mapper);
 
-            //When
             var guid = "";
             Func<Task> action = async () => await service.GetSession(guid);
 
-            //Then 
             await action.Should().ThrowAsync<Exception>().WithMessage("Session Id cannot be empty.");
         }
 
         [Fact]
         public async Task GivenNoAnswersInDictionary_WhenGetResults_ShouldReturnResultZero()
         {
-            //Given
-            var mockRepository = A.Fake<ISessionRepository>();
-            var service = new SessionService(mockRepository, _mapper);
+            var service = new SessionService(mockRepository._repo, mapperFixture._mapper);
 
-            //When
             var result = await service.GetResult(new CreateFinishSessionDtoBuilder().BuildQuizId(1).BuildSessionId("123123").Build());
 
-            //Then 
             Assert.Equal(0, result.Result);
         }
-
-
     }
 }
